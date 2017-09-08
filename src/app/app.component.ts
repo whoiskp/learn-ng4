235b88691170dfbf95a6ceba2c1848895a5f541e
Whois Kp <kpwhois@gmail.com>
2017-09-08 10:58:11 +0700

    Add promise service and slowly to get listHeroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Hello Angular 4';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
-  constructor() {
-    console.log(this.heroes);
+  constructor(private heroService: HeroService) {
   }
 
+  ngOnInit() {
+    this.getListHero();
+  }
+  getListHero(): void {
+    this.heroService.getHerosSlowly().then(
+      heroes => this.heroes = heroes
+    );
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
---
+++ src/app/hero.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+    getHeros(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
+    }
+
+    getHerosSlowly(): Promise<Hero[]> {
+        return new Promise(resolve => {
+            setTimeout(() => resolve(this.getHeros()), 2000);
+        });
+    }
+}
